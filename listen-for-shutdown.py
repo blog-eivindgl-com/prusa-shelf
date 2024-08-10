5a914ef5a6a9efb5a4d8f3ccbef66626c44687a9
Eivind Gussiås Løkseth <eivind@outlook.com>
2024-08-10 17:28:22 +0200

    Require 3s button hold before shutdown

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -4,14 +4,17 @@
 import subprocess
 import os
 from gpiozero import Button, LED
+from signal import pause
 
 #GPIO.setmode(GPIO.BCM)
 #GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 #GPIO.wait_for_edge(3, GPIO.FALLING)
-button = Button(3, hold_time=3)
-button.wait_for_press()
-led = LED(4)
-led.on()
-#subprocess.call(['shutdown', '-h', 'now'], shell=True)
-os.system("sudo shutdown -h now")
+red_led = LED(4)
+def shutdown():
+  red_led.on()
+  #subprocess.call(['shutdown', '-h', 'now'], shell=True)
+  os.system("sudo shutdown -h now")
 
+button = Button(3, hold_time=3)
+button.when_held = shutdown
+pause()
