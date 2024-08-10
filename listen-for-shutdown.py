3b2d51ba28092e98ca2ac9d5c48517965e2acb7b
Eivind Gussiås Løkseth <eivind@outlook.com>
2024-08-10 17:17:33 +0200

    Add red LED to indicate shutdown

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -3,13 +3,15 @@
 #import RPi.GPIO as GPIO
 import subprocess
 import os
-from gpiozero import Button
+from gpiozero import Button, LED
 
 #GPIO.setmode(GPIO.BCM)
 #GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 #GPIO.wait_for_edge(3, GPIO.FALLING)
-button = Button(3)
+button = Button(3, hold_time=3)
 button.wait_for_press()
+led = LED(4)
+led.on()
 #subprocess.call(['shutdown', '-h', 'now'], shell=True)
 os.system("sudo shutdown -h now")
 
